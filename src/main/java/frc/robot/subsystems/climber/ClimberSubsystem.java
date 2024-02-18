3ef0a96e65a1f047645493847e968d0794548962
kyle <75395106+ehre9305@users.noreply.github.com>
2024-02-18 10:32:50 -0600

    Predeploy autocommit for competition

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -8,6 +8,7 @@ public class ClimberSubsystem extends SubsystemBase {
   private final ClimberIO climberIO;
   private final ClimberIOInputsAutoLogged climberIOInputs = new ClimberIOInputsAutoLogged();
   private String descriptor;
+  private double volts = 0;
 
   public ClimberSubsystem(ClimberIO climberIO, String descriptor) {
     this.climberIO = climberIO;
@@ -20,13 +21,24 @@ public class ClimberSubsystem extends SubsystemBase {
   public void periodic() {
     climberIO.updateInputs(climberIOInputs);
     Logger.processInputs("climber/" + descriptor, climberIOInputs);
+    checkLimit();
   }
 
   public void stop() {
     climberIO.stop();
   }
 
+  private void checkLimit() {
+    if (climberIOInputs.atBottom && (
+        (climberIOInputs.positionRotations > ClimberConstants.FULL_EXTENSION_ROTATIONS && volts > 0) ||
+            (climberIOInputs.positionRotations < ClimberConstants.FULL_EXTENSION_ROTATIONS && volts < 0)
+    )) {
+      climberIO.setVoltage(0);
+    }
+  }
+
   public void setVoltage(double volts) {
+    this.volts = volts;
     climberIO.setVoltage(volts);
   }
 
