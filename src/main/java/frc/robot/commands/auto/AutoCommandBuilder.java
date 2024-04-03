620168e6188030ed91a281bee84c4d76b23e1fdd
kyle <75395106+ehre9305@users.noreply.github.com>
2024-04-03 13:45:02 -0500

    add ability to wait for time in auto

--- src/main/java/frc/robot/commands/auto/AutoCommandBuilder.java
+++ src/main/java/frc/robot/commands/auto/AutoCommandBuilder.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
@@ -172,6 +173,12 @@ public class AutoCommandBuilder {
               }
             });
     output.addCommands(setObstacles);
+
+    if (autoPart.time().isPresent()) {
+      output.addCommands(
+              logAutoState("waiting"),
+              Commands.waitUntil(() -> DriverStation.getMatchTime() <= autoPart.time().get()));
+    }
 
     if (autoPart.notePickupPose().isPresent()) {
       final Command driveToPickup = driveToPickup(autoPart.notePickupPose().get());
