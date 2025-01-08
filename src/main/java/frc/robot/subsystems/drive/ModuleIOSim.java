5684e0a0d369dee7305eb46ceb60fa1d5cc244b2
Chris Morton <chris.morton@certifi.com>
2025-01-07 21:41:47 -0600

    need to fix the shooter as well

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
@@ -17,7 +17,9 @@ import static frc.robot.subsystems.drive.DriveConstants.*;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
@@ -32,9 +34,14 @@ public class ModuleIOSim implements ModuleIO {
   private static final double LOOP_PERIOD_SECS = 0.02;
 
   private DCMotorSim driveSim =
-      new DCMotorSim(DCMotor.getNEO(1), moduleConstants.driveReduction(), 0.025);
+     new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNEO(1),
+         moduleConstants.driveReduction(), 0.025),
+        DCMotor.getNEO(1));
+
+
   private DCMotorSim turnSim =
-      new DCMotorSim(DCMotor.getNEO(1), moduleConstants.turnReduction(), 0.004);
+      new DCMotorSim(LinearSystemId.createDCMotorSystem(moduleConstants.turnReduction(), 0.004),
+          DCMotor.getNEO(1));
 
   private final Rotation2d turnAbsoluteInitPosition = new Rotation2d(Math.random() * 2.0 * Math.PI);
   private double driveAppliedVolts = 0.0;
